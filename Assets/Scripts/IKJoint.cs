fdbe047a547b2f263df95535c67181bcc592ac56
xenone <rosenkranz1999@gmail.com>
2021-01-23 14:53:32 -0500

    project contents. Leg fk and kinematics manager created

---
+++ Assets/Scripts/IKJoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Octahedral {
+
+  public class IKJoint : MonoBehaviour
+  {
+    // Draw gizmos in editor for range of motion
+
+    // Range of motion
+    [Header("Joint Limits")]
+
+    [SerializeField] private float xOffset;
+    [SerializeField] private float yOffset;
+    [SerializeField] private float zOffset;
+
+    [SerializeField] private float xAngleRange;
+    [SerializeField] private float yAngleRange;
+    [SerializeField] private float zAngleRange;
+
+    public Vector3 StartOffset; 
+
+    void Awake() {
+      // set our offset from the parent
+      StartOffset = transform.localPosition;
+    }
+
+
+    // void OnSceneGui() {
+    //   // X
+    //   Handles.color = new Color(0f, 1f, 0f);
+    //   Handles.DrawSolidArc(transform.position,
+    //                        transform.right,
+    //                        transform.up,
+    //                        xAngleRange,
+    //                        1.0f);
+    //   //HandleUtility.GetHandleSize(transform.position));
+    // }
+
+
+
+  }
+
+}
---
+++ Assets/Scripts/KinematicsManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Octahedral {
+  
+  /// Manages a set of joints
+  public class KinematicsManager : MonoBehaviour
+  {
+
+    public List<IKJoint> joints;
+
+    // TODO: automatically add joints in order
+    public Transform target;
+
+
+    void Update() {
+      target.position = ForwardKinematics();
+    }
+
+    public Vector3 ForwardKinematics() {
+      Vector3 prevPoint = joints[0].transform.position;
+      Quaternion rotation = Quaternion.identity;
+      for (int i = 1; i < joints.Count; i++)
+      {
+        // Rotates around a new axis
+        rotation *= joints[i - 1].transform.localRotation;
+        Vector3 nextPoint = prevPoint + rotation * joints[i].StartOffset;
+    
+        prevPoint = nextPoint;
+      }
+      return prevPoint;
+    }
+  }
+
+}
